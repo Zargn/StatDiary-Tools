b982b47424ee280b898a188045d2b37d7c2153f4
Oskar Anderson <oskaranderson99@gmail.com>
2026-01-26 10:48:12 +0100

    Manual_tests can now call the "C" function meant for external access

--- manual_tests/src/main.rs
+++ manual_tests/src/main.rs
@@ -1,3 +1,12 @@
+use std::ffi::CString;
+
 fn main() {
     println!("Hello, world!");
+
+    let path = "/tmp/my_db";
+    let c_path = CString::new(path).unwrap();
+
+    let rc = unsafe { stat_diary_tools::compress_db_to_image(c_path.as_ptr()) };
+
+    println!("Exit code: {rc}");
 }
--- stat_diary_tools/src/lib.rs
+++ stat_diary_tools/src/lib.rs
@@ -1,31 +1,28 @@
 use std::ffi::{c_char, CStr};
 
 #[no_mangle]
-pub unsafe extern "C" fn test_db(path: *const c_char) -> i32 {
+pub unsafe extern "C" fn compress_db_to_image(path: *const c_char) -> i32 {
     if path.is_null() {
         return -1;
     }
     let path = unsafe { CStr::from_ptr(path).to_string_lossy() };
 
-    compress_db_to_image(&path);
+    local_compress_db_to_image(&path);
 
     1
 }
 
-fn compress_db_to_image(db_path: &str) -> Result<(), ()> {
-    todo!();
-}
-
-pub fn tester() {
-    todo!();
+fn local_compress_db_to_image(db_path: &str) -> Result<(), ()> {
+    println!("Success! Using path: {}", db_path);
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::compress_db_to_image;
+    use super::local_compress_db_to_image;
 
     #[test]
     fn test_compress_db_to_image() {
-        compress_db_to_image("none");
+        local_compress_db_to_image("none");
     }
 }
